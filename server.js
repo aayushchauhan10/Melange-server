f3753d669e968d5f73a60e6ff7c8a2b7cb09fe60
aayushchauhan10 <aayushchauhan561@gmail.com>
2024-01-04 16:55:30 +0530

    Added client link

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://video-production.melangedigital.in/"
+    "https://video-production.melangedigital.in"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
